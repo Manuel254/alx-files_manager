a632aac3fbbac0e8caf8cc119efc996a4637dea2
Manuel254 <Immanuelkituku@gmail.com>
2023-09-03 21:30:59 +0300

    Fix returns on errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,19 +7,27 @@ const controller = {
 
       if (!email) {
         res.status(400).json({ error: 'Missing email' });
-      } else if (!password) {
+        res.end();
+        return;
+      }
+      if (!password) {
         res.status(400).json({ error: 'Missing password' });
+        res.end();
+        return;
       }
 
       const user = await db.findUser(email);
 
       if (user) {
         res.status(400).json({ error: 'Already exist' });
+        res.end();
+        return;
       }
 
       const userId = await db.createUser(email, password);
 
       res.status(201).json({ id: userId, email });
+      res.end();
     } catch (err) {
       console.error(err);
       throw err;
